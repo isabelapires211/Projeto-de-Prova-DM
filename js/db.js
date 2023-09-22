74bc403752658160f1964bb72ef9ac63f5776626
isabelapires211 <piresisabela211@gmail.com>
2023-09-21 22:51:49 -0300

    commit

--- js/db.js
+++ js/db.js
@@ -1,4 +1,3 @@
-
 import { openDB } from "idb";
 
 let db;
@@ -20,7 +19,7 @@ async function createDB() {
         });
         showResult("Banco de dados aberto.");
     } catch (e) {
-        showResult("Erro ao criar o banco de dados: " + e.message)
+        showError("Erro ao criar o banco de dados: " + e.message);
     }
 }
 
@@ -32,59 +31,79 @@ window.addEventListener("DOMContentLoaded", async event => {
 });
 
 async function getData() {
-    if (db == undefined) {
-        showResult("O banco de dados está fechado");
+    if (!db) {
+        showError("O banco de dados está fechado");
         return;
     }
 
-    const tx = await db.transaction('pessoas', 'readonly')
+    const tx = await db.transaction('pessoas', 'readonly');
     const store = tx.objectStore('pessoas');
-    const value = await store.getAll();
-    if (value) {
-        showResult("Dados do banco: " + JSON.stringify(value))
-    } else {
-        showResult("Não há nenhum dado no banco!")
+    try {
+        const value = await store.getAll();
+        if (value.length > 0) {
+            showResult("Dados do banco: " + JSON.stringify(value));
+        } else {
+            showResult("Não há nenhum dado no banco!");
+        }
+    } catch (error) {
+        showError("Erro ao buscar dados: " + error.message);
     }
 }
 
 async function addData() {
     const nomeInput = document.querySelector('input[name="nome"]');
     const idadeInput = document.querySelector('input[name="idade"]');
-    const nome = nomeInput.value;
-    const idade = idadeInput.value; 
+    const nome = nomeInput.value.trim(); // Remova espaços em branco
+    const idade = idadeInput.value.trim(); // Remova espaços em branco
 
     if (!nome || !idade) {
-        showResult("Por favor, preencha os campos Nome e Idade.");
+        showError("Por favor, preencha os campos Nome e Idade.");
         return;
     }
 
     const tx = await db.transaction('pessoas', 'readwrite');
     const store = tx.objectStore('pessoas');
-    store.add({ nome, idade });
-
-    await tx.done;
-    showResult(`Dados salvos: Nome - ${nome}, Idade - ${idade}`);
-    nomeInput.value="";
-    idadeInput.value="";
+    try {
+        await store.add({ nome, idade });
+        showResult(`Dados salvos: Nome - ${nome}, Idade - ${idade}`);
+        nomeInput.value = "";
+        idadeInput.value = "";
+    } catch (error) {
+        showError("Erro ao salvar dados: " + error.message);
+    }
 }
 
 function showResult(text) {
     document.querySelector("output").innerHTML = text;
 }
 
-async function buscar(){
-    const nomeBuscando = document.getElementById('buscarNome').value;
-    const tx = db.transaction('pessoas', 'readonly');
-    const store = tx.objectStore ('pessoas');
-     try{
-        let objetoBuscado = await store.get(nomeBuscando);
-        document.getElementById('nome').value =
-        objetoBuscado.nome
-        document.getElementById('idade').value =
-        objetoBuscado.idade
-     } catch(error){
-        console.log(error.message);
-     }
+function showError(text) {
+    console.error(text);
+    showResult(text);
 }
 
+async function buscar() {
+    const nomeBuscando = document.getElementById('buscar').value.trim(); // Remova espaços em branco
+    if (!db) {
+        showError("O banco de dados está fechado");
+        return;
+    }
 
+    const tx = db.transaction('pessoas', 'readonly');
+    const store = tx.objectStore('pessoas');
+    try {
+        let objetoBuscado = await store.get(nomeBuscando);
+        if (objetoBuscado) {
+            const nomeElement = document.getElementById('nome');
+            const idadeElement = document.getElementById('idade');
+            if (nomeElement && idadeElement) {
+                nomeElement.value = objetoBuscado.nome;
+                idadeElement.value = objetoBuscado.idade;
+            }
+        } else {
+            showResult(`Nenhum registro encontrado para o nome ${nomeBuscando}.`);
+        }
+    } catch (error) {
+        showError("Erro ao buscar dados: " + error.message);
+    }
+}
